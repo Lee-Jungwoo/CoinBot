e0c4e8f48fc1fc5edcc1942a23e7f12fe3821b81
Lee-Jungwoo <dlwjddn0225@gmail.com>
2024-01-11 02:14:14 +0900

    j

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ h = mongodb_handler.MongoDBHandler("local","coiner_test","price_info")
 
 cursor = h.find_items(h.get_current_db_name(),h.get_current_collection_name(), {})
 
-pprint.pprint(cursor)
+pprint.pprint(cursor.next())
 
 
